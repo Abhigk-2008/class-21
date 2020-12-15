499e977cbae10cdd782a31b15342b51e3e8463a9
Abhigk-2008 <75119541+Abhigk-2008@users.noreply.github.com>
2020-12-15 14:36:16 +0545

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,45 @@
-var fixedRect, movingRect;
+var fixedRect, movingRect
 
-function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400,200,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
-}
+var gameObject1, gameObject2, gameObject3, gameObject4;
 
-function draw() {
-  background(0,0,0);  
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
+function setup() { createCanvas(1200,800);
 
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
-      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
-      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
-    movingRect.shapeColor = "red";
-    fixedRect.shapeColor = "red";
+   movingRect = createSprite(400, 200, 50, 80);
+
+   movingRect.shapeColor = "green";
+    movingRect.debug = false; 
+    
+    fixedRect = createSprite(600, 400, 80, 30);
+     fixedRect.shapeColor = "green"; 
+     fixedRect.debug = false;
+     gameObject1 = createSprite(100, 100, 50, 50); 
+     gameObject1.shapeColor = "green"; 
+     gameObject2 = createSprite(200, 100, 50, 50);
+     gameObject2.shapeColor = "green";
+     gameObject3 = createSprite(300, 100, 50, 50);
+     gameObject3.shapeColor = "green";
+     gameObject4 = createSprite(400, 100, 50, 50);
+     gameObject4.shapeColor = "green";
+     }
+
+     function draw() { background(0);  
+      movingRect.x = mouseX; 
+      movingRect.y = mouseY; 
+      if (isTouching(movingRect,gameObject3)){
+        movingRect.shapeColor = "red";
+        gameObject3.shapeColor = "red";
+     }
+     else { movingRect.shapeColor = "green";
+     gameObject3.shapeColor = "green"
+       drawSprites(); }
+
+function isTouching(Object1,Object2) { 
+
+  if (Object1.x-Object2.x > Object2.width/2+Object1.width/2)
+  {return true
+ } 
+ else {
+return false
   }
-  else {
-    movingRect.shapeColor = "green";
-    fixedRect.shapeColor = "green";
-  }
-  drawSprites();
+
 }
